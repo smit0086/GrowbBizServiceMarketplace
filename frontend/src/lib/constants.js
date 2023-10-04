e75be4b1073520c98cbd20ebee375ec1608ecaec
Smit Patel <sm537103@dal.ca>
2023-10-04 18:45:51 -0300

    feat: init shadcn and create partner login UI

---
+++ frontend/src/lib/constants.js
@@ -0,0 +1,7 @@
+export const COPY = {
+    APP_NAME: "GrowBiz",
+    APP_TAGLINE: "Empowering Local Dreams, Growing Local Businesses.",
+    PARTNER_TESTIMONIAL:
+        "GrowBiz works wonders! Our sales have soared since using it. Highly recommended!",
+    PARTNER_NAME: "John Doe",
+};
