119e67e17bc3ee420d5829644caf8e13baadd142
vivek <viveksonani302@gmail.com>
2023-10-05 17:24:55 -0300

    feat: Created sign-up page UI for customer and added validation for sign-up form.

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,11 @@
 import { COPY } from "@/lib/constants";
-import CustomerLogin from "@/app/login/page"
+// import CustomerLogin from "@/app/login/page"
 
 export default function Home() {
-    // return <main>{COPY.APP_NAME}</main>;
-    return(
-        <main>
-            <CustomerLogin />
-        </main>
-    );
+    return <main>{COPY.APP_NAME}</main>;
+    // return(
+    //     <main>
+    //         <CustomerLogin />
+    //     </main>
+    // );
 }
