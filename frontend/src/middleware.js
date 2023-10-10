bfb3619890487b8898f1907bff176f6a258d3f00
Smit Patel <sm537103@dal.ca>
2023-10-10 14:42:35 -0300

    feat: issue-33 role based redirection

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,19 +1,21 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 // middleware is applied to all routes, use conditionals to select
 const protected_routes = {
     CUSTOMER: ["/dashboard"],
     PARTNER: ["/partner/dashboard"],
-    ADMIN: [["/admin/dashboard"]],
+    ADMIN: ["/admin/dashboard"],
 };
+
 const flattened_protected_routes = [
     ...protected_routes.CUSTOMER,
     ...protected_routes.PARTNER,
     ...protected_routes.ADMIN,
 ];
-export default withAuth(function middleware(req) {}, {
-    callbacks: {
-        authorized: ({ req, token }) => {
+export default withAuth(
+    function middleware(req) {
+        if (req.nextauth?.token?.role) {
             const isVisitingProtectedRoute = flattened_protected_routes.reduce(
                 (prev, curr) => {
                     return (
@@ -24,10 +26,36 @@ export default withAuth(function middleware(req) {}, {
                 },
                 false
             );
-            if (isVisitingProtectedRoute && !token) {
-                return false;
+            const isPageAllowedForRole = protected_routes[
+                req.nextauth.token.role
+            ].reduce((prev, curr) => {
+                return (
+                    prev ||
+                    req.nextUrl.pathname === "/" ||
+                    req.nextUrl.pathname.startsWith(curr)
+                );
+            }, false);
+            if (!isVisitingProtectedRoute || !isPageAllowedForRole) {
+                return NextResponse.redirect(new URL("/", req.url));
             }
-            return true;
-        },
+        }
     },
-});
+    {
+        callbacks: {
+            authorized: ({ req, token }) => {
+                const isVisitingProtectedRoute =
+                    flattened_protected_routes.reduce((prev, curr) => {
+                        return (
+                            prev ||
+                            req.nextUrl.pathname === "/" ||
+                            req.nextUrl.pathname.startsWith(curr)
+                        );
+                    }, false);
+                if (isVisitingProtectedRoute && !token) {
+                    return false;
+                }
+                return true;
+            },
+        },
+    }
+);
