b456a895506a269aa62e526bdd12085f66249a18
Smit Patel <sm537103@dal.ca>
2023-10-12 14:18:58 -0300

    ci: fix nextauth redirect issue

--- setup_env.sh
+++ setup_env.sh
@@ -2,3 +2,4 @@
 
 echo SERVER_ADDRESS=$FRONTEND_SERVER_ADDRESS >> ./frontend/.env
 echo NEXTAUTH_SECRET=$FRONTEND_NEXTAUTH_SECRET >> ./frontend/.env
+echo NEXTAUTH_URL=$FRONTEND_NEXTAUTH_URL >> ./frontend/.env
