45efa36a3b43b60edc0b7220458c30e5979cc70d
vivek <viveksonani302@gmail.com>
2023-10-17 17:50:46 -0300

    fix: build error solved.

--- frontend/src/services/businessService.js
+++ frontend/src/services/businessService.js
@@ -1,4 +1,3 @@
-<<<<<<< frontend/src/services/businessService.js
 export const createBusiness = async (
     businessName,
     email,
@@ -18,7 +17,7 @@ export const createBusiness = async (
     formData.append("business", JSON.stringify(businessData));
     try {
         const resp = await (
-            await fetch(`${NEXT_PUBLIC_SERVER_ADDRESS}/api/business/save`, {
+            await fetch(`${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/business/save`, {
                 method: "POST",
                 body: formData,
                 headers: {
@@ -169,90 +168,3 @@ export const downloadDocumentByEmail = async (token, email) => {
         console.log("err", err);
     }
 };
-=======
-export const createBusiness = async (
-    businessName,
-    email,
-    categoryId,
-    role,
-    verificationDocuments,
-    token
-) => {
-    const businessData = {
-        businessName: businessName,
-        email: email,
-        categoryId: categoryId,
-        role: role,
-    };
-    const formData = new FormData();
-    formData.append("file", verificationDocuments);
-    formData.append("business", JSON.stringify(businessData));
-    try {
-        const resp = await (
-            await fetch(`${NEXT_PUBLIC_SERVER_ADDRESS}/api/business/save`, {
-                method: "POST",
-                body: formData,
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-        ).json();
-        return resp;
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-export const getBusiness = async (email, token) => {
-    const resp = await (
-        await fetch(
-            `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/business/?email=${email}`,
-            {
-                method: "get",
-                headers: {
-                    "Content-type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        )
-    ).json();
-    return resp;
-};
-
-export const updateBusiness = async (
-    businessName,
-    email,
-    categoryId,
-    role,
-    verificationDocuments,
-    token,
-    businessId
-) => {
-    const businessData = {
-        businessName: businessName,
-        email: email,
-        categoryId: categoryId,
-        role: role,
-    };
-    const formData = new FormData();
-    formData.append("file", verificationDocuments);
-    formData.append("business", JSON.stringify(businessData));
-    try {
-        const resp = await (
-            await fetch(
-                `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/business/update/${businessId}`,
-                {
-                    method: "PUT",
-                    body: formData,
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            )
-        ).json();
-        return resp;
-    } catch (err) {
-        console.error(err);
-    }
-};
->>>>>>> frontend/src/services/businessService.js
