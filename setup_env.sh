17501dc3dbd5342c39ee12385da6398c2850ef12
Prithvi Manoj Krishna <pr732042@dal.ca>
2023-10-18 16:34:55 -0300

    ci: add environment variables for java runtime

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Setup environment variables for frontend
 echo NEXT_PUBLIC_SERVER_ADDRESS=$FRONTEND_SERVER_ADDRESS >> ./frontend/.env
 echo NEXTAUTH_SECRET=$FRONTEND_NEXTAUTH_SECRET >> ./frontend/.env
 echo NEXTAUTH_URL=$FRONTEND_NEXTAUTH_URL >> ./frontend/.env
+
+# Setup environment variables for docker compose deployment
+echo JDBC_USERNAME=${JDBC_USERNAME} >> ./.env
+echo JDBC_URL=${JDBC_URL} >> ./.env
+echo JDBC_PASSWORD=${JDBC_PASSWORD} >> ./.env
+echo GROWBIZ_SECRET_KEY=${GROWBIZ_SECRET_KEY} >> ./.env
