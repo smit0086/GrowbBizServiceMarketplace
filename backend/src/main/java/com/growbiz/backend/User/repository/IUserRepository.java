064966dda41550cc6d4af98e45dc53a089341e6e
Prithvi Manoj Krishna <pr732042@dal.ca>
2023-10-19 18:11:16 -0300

    chore: add repository annotation to IUserRepository

--- backend/src/main/java/com/growbiz/backend/User/repository/IUserRepository.java
+++ backend/src/main/java/com/growbiz/backend/User/repository/IUserRepository.java
@@ -3,7 +3,9 @@ package com.growbiz.backend.User.repository;
 import com.growbiz.backend.User.models.Role;
 import com.growbiz.backend.User.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends CrudRepository<User, Long> {
     public User findByEmailAndRole(String email, Role role);
 }
