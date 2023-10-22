650e7faf59b19ffb6585061b04fd4166445df2df
rabiaa <rb618118@dal,ca>
2023-10-22 15:19:16 -0300

    chore: made some changes to application.yaml and securityconfiguration

--- backend/src/main/java/com/growbiz/backend/Security/config/SecurityConfiguration.java
+++ backend/src/main/java/com/growbiz/backend/Security/config/SecurityConfiguration.java
@@ -50,16 +50,4 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-
 }
