dd4161bc7b045d4e7e7bddae82ea9592c578bbf0
Smit Patel <sm537103@dal.ca>
2023-10-24 19:48:22 -0300

    feat: add sidebar navigation on admin

--- frontend/src/app/admin/(Sidebar)/business/verify/page.jsx
+++ frontend/src/app/admin/(Sidebar)/business/verify/page.jsx
@@ -1,8 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-import PendingBusinessesLoader from "@/app/admin/business/verify/components/PendingBusinessesLoader";
-import Navbar from "@/components/modules/Navbar/Navbar";
+import PendingBusinessesLoader from "./components/PendingBusinessesLoader";
 import { getAllCategories } from "@/services/categoryService";
 
 export default async function BusinessApproval() {
--- frontend/src/app/admin/(Sidebar)/dashboard/page.jsx
+++ frontend/src/app/admin/(Sidebar)/dashboard/page.jsx
@@ -1,7 +1,9 @@
 import Navbar from "@/components/modules/Navbar/Navbar";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const AdminDashboard = () => {
+    redirect("/admin/business/verify");
     return (
         <>
             <div>AdminDashboard</div>
