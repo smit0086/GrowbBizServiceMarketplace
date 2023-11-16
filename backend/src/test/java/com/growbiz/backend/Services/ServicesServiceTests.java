dbff6b0efee61bdb77171e1190e142b8b3b58578
rabiaa <rb618118@dal,ca>
2023-11-16 16:45:32 -0400

    feat: service service layer tests complete

--- backend/src/test/java/com/growbiz/backend/Services/ServicesServiceTests.java
+++ backend/src/test/java/com/growbiz/backend/Services/ServicesServiceTests.java
@@ -6,26 +6,20 @@ import com.growbiz.backend.Categories.models.Category;
 import com.growbiz.backend.Categories.models.SubCategory;
 import com.growbiz.backend.Categories.service.Sub.ISubCategoryService;
 import com.growbiz.backend.Categories.service.Super.ICategoryService;
-import com.growbiz.backend.File.service.IFileStorageService;
 import com.growbiz.backend.Services.models.ServiceRequest;
 import com.growbiz.backend.Services.models.Services;
 import com.growbiz.backend.Services.repository.IServiceRepository;
 import com.growbiz.backend.Services.service.ServicesService;
-import com.growbiz.backend.TestConstants.TestConstants;
 
+import com.growbiz.backend.TestConstants.TestConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,9 +38,6 @@ public class ServicesServiceTests {
     private ICategoryService categoryService;
     @Mock
     private IBusinessService businessService;
-    @Mock
-    private IFileStorageService fileStorageService;
-
     Business mockBusiness;
 
     Category mockCategory;
@@ -66,45 +57,48 @@ public class ServicesServiceTests {
         mockCategory = Category
                 .builder()
                 .categoryID(1L)
-                .name("Beauty & Aesthetics")
-                .tax("15")
+                .name(TestConstants.TEST_CATEGORY_NAME)
+                .tax(TestConstants.TEST_CATEGORY_TAX)
                 .build();
         mockSubCategory = SubCategory
                 .builder()
                 .subCategoryID(1L)
-                .name("Manicure")
+                .name(TestConstants.TEST_SUBCATEGORY_NAME)
                 .category(mockCategory)
                 .build();
         mockBusiness = Business
                 .builder()
                 .businessId(1L)
-                .businessName("French Nails")
+                .businessName(TestConstants.TEST_BUSINESS_NAME)
+                .email(TestConstants.TEST_EMAIL)
                 .build();
         mockServiceRequest = ServiceRequest
                 .builder()
-                .email("testEmail@dal.ca")
-                .serviceName("Nail Care")
-                .description("Loren Epsom")
-                .price(24.00)
+                .email(TestConstants.TEST_EMAIL)
+                .serviceName(TestConstants.TEST_SERVICE_NAME)
+                .description(TestConstants.TEST_SERVICE_DESCRIPTION)
+                .price(TestConstants.TEST_SERVICE_PRICE)
                 .businessID(1)
                 .subCategoryID(1)
+                .image(TestConstants.TEST_SERVICE_IMAGE_URL)
                 .build();
         mockServiceUpdated = Services
                 .builder()
                 .serviceId(1L)
-                .serviceName("Hands and Nail Care")
-                .description("Loren Epsom")
-                .price(32.00)
+                .serviceName(TestConstants.TEST_SERVICE_NAME)
+                .description(TestConstants.TEST_SERVICE_DESCRIPTION)
+                .price(TestConstants.TEST_SERVICE_PRICE_UPDATED)
                 .business(mockBusiness)
                 .subCategory(mockSubCategory)
+                .imageURL(TestConstants.TEST_SERVICE_IMAGE_URL)
                 .build();
         mockService = Services
                 .builder()
                 .serviceId(1L)
-                .serviceName("Nail Care")
-                .description("Loren Epsom")
+                .serviceName(TestConstants.TEST_SERVICE_NAME)
+                .description(TestConstants.TEST_SERVICE_DESCRIPTION)
                 .price(24.00)
-                .imageURL("image.jpeg")
+                .imageURL(TestConstants.TEST_SERVICE_IMAGE_URL)
                 .business(mockBusiness)
                 .subCategory(mockSubCategory)
                 .build();
@@ -147,6 +141,69 @@ public class ServicesServiceTests {
     }
 
     @Test
+    public void getServicesByCategoryIdTest() {
+
+    }
+
+    @Test
+    public void addNewServiceTest() {
+        Services toBeSavedService = Services
+                .builder()
+                .serviceName(TestConstants.TEST_SERVICE_NAME)
+                .description(TestConstants.TEST_SERVICE_DESCRIPTION)
+                .price(24.00)
+                .imageURL(TestConstants.TEST_SERVICE_IMAGE_URL)
+                .business(mockBusiness)
+                .subCategory(mockSubCategory)
+                .build();
+        when(serviceRepository.findByServiceNameAndBusinessBusinessId(TestConstants.TEST_SERVICE_NAME, 1L)).thenReturn(null);
+        when(businessService.findById(1L)).thenReturn(mockBusiness);
+        when(subCategoryService.getSubCategoryByID(1L)).thenReturn(mockSubCategory);
+        doReturn(mockService).when(serviceRepository).save(toBeSavedService);
+        Services results = servicesService.addService(mockServiceRequest);
+
+        assertEquals(mockService,results);
+        verify(serviceRepository, times(1)).save(any(Services.class));
+    }
+
+    @Test
+    public void addExistingServiceTest() {
+        when(serviceRepository.findByServiceNameAndBusinessBusinessId(TestConstants.TEST_SERVICE_NAME, 1L)).thenReturn(mockService);
+        Services results = servicesService.addService(mockServiceRequest);
+        assertNull(results);
+    }
+
+    @Test
+    public void updateExistingServiceTest() {
+        ServiceRequest toBeUpdatedService = ServiceRequest
+                .builder()
+                .serviceName(TestConstants.TEST_SERVICE_NAME)
+                .description(TestConstants.TEST_SERVICE_DESCRIPTION)
+                .price(TestConstants.TEST_SERVICE_PRICE_UPDATED)
+                .image(TestConstants.TEST_SERVICE_IMAGE_URL)
+                .businessID(1L)
+                .subCategoryID(1L)
+                .build();
+        when(serviceRepository.findById(anyLong())).thenReturn(Optional.of(mockService));
+        when(businessService.findById(1L)).thenReturn(mockBusiness);
+        when(subCategoryService.getSubCategoryByID(1L)).thenReturn(mockSubCategory);
+        when(serviceRepository.save(mockServiceUpdated)).thenReturn(mockServiceUpdated);
+
+        Services results = servicesService.updateService(toBeUpdatedService);
+
+        assertEquals(mockServiceUpdated,results);
+        verify(serviceRepository, times(1)).save(any(Services.class));
+    }
+
+    @Test
+    public void updateNonExistingServiceTest() {
+        Optional<Services> optionalService = Optional.empty();
+        when(serviceRepository.findById(anyLong())).thenReturn(optionalService);
+        Services results = servicesService.updateService(mockServiceRequest);
+        assertNull(results);
+    }
+
+    @Test
     public void getTaxForServiceTest() {
         when(categoryService.getCategoryByID(1L)).thenReturn(mockCategory);
         String results = servicesService.getTaxForService(mockService);
@@ -158,21 +215,6 @@ public class ServicesServiceTests {
         when(categoryService.getCategoryByID(1L)).thenThrow(new NullPointerException("Test Exception"));
         String results = servicesService.getTaxForService(mockService);
         assertNull(results);
-    }
-
-    @Test
-    public void deleteExistingServiceTest() {
-        doNothing().when(serviceRepository).deleteById(mockService.getServiceId());
-        Boolean result = servicesService.deleteService(mockService.getServiceId());
-        verify(serviceRepository, times(1)).deleteById(mockService.getServiceId());
-        assertTrue(result);
-    }
-
-    @Test
-    public void deleteNonExistingServiceTest() {
-        doThrow(new NullPointerException("Test Exception")).when(serviceRepository).deleteById(mockService.getServiceId());
-        Boolean result = servicesService.deleteService(mockService.getServiceId());
-        assertFalse(result);
     }
 
     @Test
@@ -189,4 +231,18 @@ public class ServicesServiceTests {
         assertNull(services);
     }
 
+    @Test
+    public void deleteExistingServiceTest() {
+        doNothing().when(serviceRepository).deleteById(mockService.getServiceId());
+        Boolean result = servicesService.deleteService(mockService.getServiceId());
+        verify(serviceRepository, times(1)).deleteById(mockService.getServiceId());
+        assertTrue(result);
+    }
+
+    @Test
+    public void deleteNonExistingServiceTest() {
+        doThrow(new NullPointerException("Test Exception")).when(serviceRepository).deleteById(mockService.getServiceId());
+        Boolean result = servicesService.deleteService(mockService.getServiceId());
+        assertFalse(result);
+    }
 }
