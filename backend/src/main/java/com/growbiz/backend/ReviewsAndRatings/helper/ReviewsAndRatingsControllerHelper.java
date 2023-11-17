f044e52aa108abbac49a1f7572dc1ca8129f692f
rabiaa <rb618118@dal,ca>
2023-11-17 17:55:48 -0400

    feat: added helper package for ReviewsAndRatings and removed unnecessary imports for servicehelpercontroller

---
+++ backend/src/main/java/com/growbiz/backend/ReviewsAndRatings/helper/ReviewsAndRatingsControllerHelper.java
@@ -0,0 +1,30 @@
+package com.growbiz.backend.ReviewsAndRatings.helper;
+
+import com.growbiz.backend.ReviewsAndRatings.models.ReviewsAndRatings;
+import com.growbiz.backend.ReviewsAndRatings.models.ReviewsAndRatingsResponse;
+import com.growbiz.backend.User.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ReviewsAndRatingsControllerHelper {
+    public ResponseEntity<ReviewsAndRatingsResponse> createReviewsAndRatingsResponse(List<ReviewsAndRatings> ReviewsAndRatingsList, Boolean isUpdated) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(ReviewsAndRatingsResponse.builder()
+                .build());
+    }
+
+    public ResponseEntity<ReviewsAndRatingsResponse> deleteReviewsAndRatingsResponse(Boolean deleted) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(ReviewsAndRatingsResponse.builder()
+                .isDeleted(deleted)
+                .subject(user.getEmail())
+                .role(user.getRole())
+                .build());
+    }
+}
+
+
