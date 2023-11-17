3dee2d2390e72a39027ec2f088332eb2aabc8d25
rabiaa <rb618118@dal,ca>
2023-11-17 18:26:27 -0400

    feat: added repository package for ReviewsAndRatings

---
+++ backend/src/main/java/com/growbiz/backend/ReviewsAndRatings/repository/IReviewsAndRatingsRepository.java
@@ -0,0 +1,15 @@
+package com.growbiz.backend.ReviewsAndRatings.repository;
+
+import com.growbiz.backend.ReviewsAndRatings.models.ReviewsAndRatings;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IReviewsAndRatingsRepository extends CrudRepository<ReviewsAndRatings, Long> {
+
+    List<ReviewsAndRatings> findByUserId(Long userId);
+
+    List<ReviewsAndRatings> findByServiceServiceId(Long serviceId);
+}
